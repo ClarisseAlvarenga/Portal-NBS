a5bb1ae8a68456f7fd39896704617f5bff289739
Ezequiel <ezequiel1de1oliveira@gmail.com>
2019-02-12 12:56:29 -0200

    subindo skos somente no primeiro build
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    
    Co-authored-by: julliana <julliana.coutoalmeida@gmail.com>
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Nitai <nitaibezerra@protonmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+file="populado.txt"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+    rake iqvoc:import:url URL=data/NBS2-skos.nt NAMESPACE='http://vocab.mdic.gov.br/NBS/v2.0/'
+    echo "Banco Populado" > populado.txt
+fi
+
 rake assets:precompile && bundle exec rake db:migrate && bundle exec rake db:seed && bin/delayed_job start && exec bundle exec passenger start --port 3000 --environment $RAILS_ENV
