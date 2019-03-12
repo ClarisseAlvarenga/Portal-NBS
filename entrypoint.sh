f6ddac42c9d28709342038c7fc786ae3a3a365af
Ezequiel <ezequiel1de1oliveira@gmail.com>
2019-03-12 13:08:24 -0300

    Adicionando .nt novo com as seções
    Signed-off-by: Ezequiel <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: julliana <julliana.coutoalmeida@gmail.com>
    Co-authored-by: Guilherme <guibanci@gmail.com>
    Co-authored-by: Nitai <nitaibezerra@protonmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,11 @@ else
     rake db:migrate
     rake iqvoc:import:url URL=data/NBS2-skos.nt NAMESPACE='http://vocab.mdic.gov.br/NBS/v2.0/'
     echo "Banco Populado" > populado.txt
+    export PGPASSWORD='iqvoc' && psql -h db -p 5432 -U iqvoc iqvoc_production -c 'CREATE EXTENSION unaccent;'
 fi
 
 rake assets:precompile
 bundle exec rake db:migrate
 bundle exec rake db:seed
 bin/delayed_job start
-export PGPASSWORD='iqvoc' && psql -h db -p 5432 -U iqvoc iqvoc_production -c 'CREATE EXTENSION unaccent;'
 exec bundle exec passenger start --port 3000 --environment $RAILS_ENV
